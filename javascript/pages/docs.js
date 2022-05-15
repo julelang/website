e3148fa764891f11c2840470bc291aada0363fe7
mertcandav <mertcandav@outlook.com>
2022-05-15 11:41:47 +0300

    Update docs.

--- javascript/pages/docs.js
+++ javascript/pages/docs.js
@@ -3058,6 +3058,17 @@ Sets n value of pointer segments to specified expression.
 <br><br>
 Special case is:
 <li><x class="inline_code">memset[Alloc_T](ptr, expr, n) = does nothing if n == 0</x></li>
+<li><x class="inline_code">memset[Alloc_T](ptr, expr, n) = does nothing if ptr == nil</x></li>
+
+<div class="topic-seperator"></div>
+<div class="code">type[Alloc_T]
+pub memcopy(dest, src *Alloc_T, const n size)</div>
+Copies n value from source allocation to destination allocation.
+<br><br>
+Special case is:
+<li><x class="inline_code">memcopy[Alloc_T](dest, src, n) = does nothing if n == 0</x></li>
+<li><x class="inline_code">memcopy[Alloc_T](dest, src, n) = does nothing if dest == nil</x></li>
+<li><x class="inline_code">memcopy[Alloc_T](dest, src, n) = does nothing if src == nil</x></li>
 `;
 
 const TAB_stdlib_os_globals = `
