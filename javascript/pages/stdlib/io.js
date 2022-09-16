2815b91f791b8445a29ba96ef5cf84bb16b6fa17
mertcandav <mertcandav@outlook.com>
2022-09-16 11:42:53 +0300

    docs: update to latest

--- javascript/pages/stdlib/io.js
+++ javascript/pages/stdlib/io.js
@@ -1,10 +1,6 @@
 const stdlib_io_functionsHTML = `
 <div class="code">fn readln() str</div>
 Reads full-complete line from command-line.
-
-<div class="topic-separator"></div>
-<div class="code">fn readin() str</div>
-Read first part of line from command-line.
 `;
 
 const NAV_stdlib_io_functions = document.getElementById("functions");
