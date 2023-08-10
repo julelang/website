9e4bab035b9c4f2ac0fafe2a8dad42d7a2d97fc0
lareii <lareii@proton.me>
2023-08-10 19:10:33 +0300

    add name property to each route

--- src/router/index.js
+++ src/router/index.js
@@ -5,26 +5,32 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      name: "home",
       path: "/",
       component: () => import("../views/Home.vue")
     },
     {
+      name: "contribute",
       path: "/contribute",
       component: () => import("../views/Contribute.vue")
     },
     {
+      name: "community",
       path: "/community",
       component: () => import("../views/Community.vue")
     },
     {
+      name: "code-of-conduct",
       path: "/code-of-conduct",
       component: () => import("../views/CodeOfConduct.vue")
     },
     {
+      name: "downloads",
       path: "/downloads",
       component: () => import("../views/Downloads.vue")
     },
     {
+      name: "404",
       path: "/:pathMatch(.*)*",
       component: () => import("../views/404.vue")
     },
