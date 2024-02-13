e3bdaf45072f00379bc293088a97f9dff801c942
mertcandav <mertcandav@outlook.com>
2024-02-13 15:47:09 +0300

    update 404 page

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,9 +1,11 @@
 <template>
-  <main class="max-w-screen-lg mx-auto px-5 py-20">
-    <div class="text-8xl font-bold">404</div>
-    <div class="text-2xl font-bold">Page Not Found</div>
-    <div class="font-semibold">Oops! Looks like the page doesn't exist.<br>If this is a mistake, please report an <a href="https://github.com/julelang/website/issues/new">issue on GitHub</a>.</div>
-  </main>    
+  <center>
+    <main class="max-w-screen-lg mx-auto px-5 py-20">
+      <div class="text-8xl font-bold">404</div>
+      <div class="mt-2 text-4xl font-bold">Page Not Found</div>
+      <div class="mt-10 font-semibold">Oops! Looks like the page doesn't exist.<br>If this is a mistake, please report an <a href="https://github.com/julelang/website/issues/new">issue on GitHub</a>.</div>
+    </main>
+  </center>
 </template>
 
 <style scoped>
