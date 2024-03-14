ceb13811fc5373b7537c4d79ecb9e267b2236fb6
mertcandav <mertcandav@outlook.com>
2024-03-14 14:27:03 +0300

    update footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -3,16 +3,32 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-  <footer class="max-w-screen-lg mx-auto p-5 flex max-md:flex-col justify-between items-center gap-5 text-sm">
-    <router-link to="/">
-        <img class="w-10" src="https://raw.githubusercontent.com/julelang/resources/master/jule_icon.png">
-      </router-link>
-    <div class="flex flex-wrap justify-center gap-5">
-      <router-link to="/downloads">Download</router-link>
-      <a href="https://manual.jule.dev" target="_blank">Manual</a>
-      <router-link to="/community">Community</router-link>
-      <router-link to="/code-of-conduct">Code of Conduct</router-link>
+  <footer>
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+      <div class="sm:flex sm:items-center sm:justify-between">
+          <router-link to="/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+              <img src="https://raw.githubusercontent.com/julelang/resources/master/jule_icon.svg" class="h-9" alt="Jule Logo" />
+              <span class="self-center text-xl text-[var(--color-primary)] font-semibold whitespace-nowrap">Jule</span>
+          </router-link>
+          <ul class="flex flex-wrap items-center gap-5 mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+              <li>
+                  <router-link to="/downloads" class="hover:underline">Download</router-link>
+              </li>
+              <li>
+                  <a class="hover:underline" href="https://manual.jule.dev" target="_blank">Manual</a>
+              </li>
+              <li>
+                  <router-link to="/community" class="hover:underline">Community</router-link>
+              </li>
+              <li>
+                  <router-link to="/code-of-conduct" class="hover:underline">Code of Conduct</router-link>
+              </li>
+          </ul>
+      </div>
+      <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+      <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        Copyright © 2023 The Jule Team<br><a href="https://github.com/julelang/jule" target="_blank" class="underline">Source code</a> is licensed under <a href="https://github.com/julelang/jule/blob/master/LICENSE" target="_blank" class="underline">BSD-3-Clause license</a>
+      </span>
     </div>
-    <div class="text-right max-md:text-center">Copyright © 2023 The Jule Team<br><a href="https://github.com/julelang/jule" target="_blank" class="underline">Source code</a> is licensed under <a href="https://opensource.org/license/bsd-3-clause/" target="_blank" class="underline">BSD-3-Clause license</a></div>
   </footer>
 </template>
