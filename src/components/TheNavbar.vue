e986e4354da7a22c37c5ffd19c1d69a5c7acbd99
mertcandav <mertcandav@outlook.com>
2024-03-18 17:43:20 +0300

    use semibold font for navbar

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -8,7 +8,7 @@ import { RouterLink } from 'vue-router';
       <router-link to="/">
         <img class="w-14" src="https://raw.githubusercontent.com/julelang/resources/master/jule_icon.png" alt="jule icon">
       </router-link>
-      <div class="flex flex-wrap justify-center gap-10">
+      <div class="flex flex-wrap justify-center gap-10 font-semibold">
         <a href="https://manual.jule.dev" target="_blank" class="hover:bg-[teal] duration-[0.3s] rounded-full pt-1 pl-2 pr-2 pb-1">Manual</a>
         <router-link to="/community" class="hover:bg-[teal] duration-[0.3s] rounded-full pt-1 pl-2 pr-2 pb-1">Community</router-link>
         <router-link to="/contribute" class="hover:bg-[teal] duration-[0.3s] rounded-full pt-1 pl-2 pr-2 pb-1">Contribute</router-link>
