559274073e77ec615487d445b818d9b97f2b60c7
mertcandav <mertcandav@outlook.com>
2024-03-18 17:27:51 +0300

    add alt attribute to navbar jule icon

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -6,7 +6,7 @@ import { RouterLink } from 'vue-router';
   <nav class="bg-[var(--bg-primary)] text-white">
     <div class="max-w-screen-lg mx-auto py-3 px-5 flex justify-between items-center">
       <router-link to="/">
-        <img class="w-14" src="https://raw.githubusercontent.com/julelang/resources/master/jule_icon.png">
+        <img class="w-14" src="https://raw.githubusercontent.com/julelang/resources/master/jule_icon.png" alt="jule icon">
       </router-link>
       <div class="flex flex-wrap justify-center gap-10">
         <a href="https://manual.jule.dev" target="_blank" class="hover:bg-[teal] duration-[0.3s] rounded-full pt-1 pl-2 pr-2 pb-1">Manual</a>
